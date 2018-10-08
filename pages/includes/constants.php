717614f3dd2e01aab02769487f83ac82ac815fc5
Unknown <2016.khushboo.chandnani@ves.ac.in>
2018-10-08 17:21:31 +0530

    added git ignore

--- pages/includes/constants.php
+++
@@ -1,9 +0,0 @@
-<?php
-// require_once("status-constants.php");
-
-define("SERVER","localhost");
-define("USER","root");
-define("PASSWORD","");
-define("DB","ngo");
-define("BASE_SERVER","localhost");
-?>
