fc038984008472ac9202b2c1883e0087d5b24da6
Unknown <2016.khushboo.chandnani@ves.ac.in>
2018-10-08 21:31:18 +0530

    populated database updates

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 08, 2018 at 02:15 PM
+-- Generation Time: Oct 08, 2018 at 05:59 PM
 -- Server version: 10.1.34-MariaDB
 -- PHP Version: 7.2.7
 
@@ -34,6 +34,14 @@ CREATE TABLE `admins` (
   `A_EMAIL` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`AID`, `ANAME`, `A_EMAIL`) VALUES
+(1, 'Nicholas', 'lacus.Mauris@auctornon.edu'),
+(5, 'Scott', 'feugiat.tellus@tellus.ca');
+
 -- --------------------------------------------------------
 
 --
@@ -47,6 +55,15 @@ CREATE TABLE `donations` (
   `D_DESCRIPTION` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `donations`
+--
+
+INSERT INTO `donations` (`DNID`, `DAMOUNT`, `NID`, `D_DESCRIPTION`) VALUES
+(1, 1000, 1, 'Educate poor'),
+(2, 2000, 2, 'Help students'),
+(3, 3000, 3, 'Awareness');
+
 -- --------------------------------------------------------
 
 --
@@ -58,6 +75,18 @@ CREATE TABLE `donationtransaction` (
   `DNID` int(11) DEFAULT NULL,
   `DID` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `donationtransaction`
+--
+
+INSERT INTO `donationtransaction` (`DTID`, `DNID`, `DID`) VALUES
+(1, 1, 2),
+(2, 2, 4),
+(3, 3, 9),
+(4, 1, 4),
+(5, 2, 9),
+(6, 3, 2);
 
 -- --------------------------------------------------------
 
@@ -72,6 +101,15 @@ CREATE TABLE `donors` (
   `D_EMAIL` varchar(100) DEFAULT NULL,
   `USER_LEVEL` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `donors`
+--
+
+INSERT INTO `donors` (`DID`, `DNAME`, `DADDRESS`, `D_EMAIL`, `USER_LEVEL`) VALUES
+(2, 'Bertha', NULL, 'Suspendisse.aliquet.sem@mauris.net', NULL),
+(4, 'Kim', NULL, 'pharetra.felis.eget@cursus.edu', NULL),
+(9, 'Grant', NULL, 'pede.ultrices@ullamcorperDuis.ca', NULL);
 
 -- --------------------------------------------------------
 
@@ -93,7 +131,9 @@ CREATE TABLE `ngo` (
 --
 
 INSERT INTO `ngo` (`NID`, `ORGNAME`, `OPASS`, `FIELD`, `OADDRESS`, `N_EMAIL`) VALUES
-(1, 'YourNGO', NULL, NULL, NULL, 'YourNGO@gmail.com');
+(1, 'YourNGO', NULL, NULL, NULL, 'YourNGO@gmail.com'),
+(3, 'YourStoryNGO', NULL, NULL, NULL, 'YourStoryNGO@gmail.com'),
+(4, 'WeNGO', NULL, NULL, NULL, 'WeNGO@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -107,6 +147,15 @@ CREATE TABLE `ngoevents` (
   `E_DESCRIPTION` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `ngoevents`
+--
+
+INSERT INTO `ngoevents` (`EID`, `NID`, `E_DESCRIPTION`) VALUES
+(1, 1, 'Educate poor'),
+(2, 2, 'Help students'),
+(3, 3, 'Awareness');
+
 -- --------------------------------------------------------
 
 --
@@ -118,6 +167,21 @@ CREATE TABLE `ngoeventspartcpn` (
   `EID` int(11) DEFAULT NULL,
   `VID` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `ngoeventspartcpn`
+--
+
+INSERT INTO `ngoeventspartcpn` (`EPID`, `EID`, `VID`) VALUES
+(1, 1, 3),
+(2, 2, 6),
+(3, 3, 7),
+(4, 1, 8),
+(5, 2, 10),
+(6, 3, 10),
+(7, 1, 6),
+(8, 2, 7),
+(9, 3, 3);
 
 -- --------------------------------------------------------
 
@@ -161,6 +225,17 @@ CREATE TABLE `volunteers` (
   `V_EMAIL` varchar(100) DEFAULT NULL,
   `USER_LEVEL` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `volunteers`
+--
+
+INSERT INTO `volunteers` (`VID`, `VNAME`, `VADDRESS`, `V_EMAIL`, `USER_LEVEL`) VALUES
+(3, 'Thomas', NULL, 'ut.mi.Duis@purusactellus.org', NULL),
+(6, 'Althea', NULL, 'mauris.Morbi.non@nequeNullam.edu', NULL),
+(7, 'Aspen', NULL, 'Suspendisse.tristique@turpisegestasFusce.com', NULL),
+(8, 'Emmanuel', NULL, 'ipsum.nunc@sitamet.com', NULL),
+(10, 'Kenneth', NULL, 'egestas.ligula@infaucibus.edu', NULL);
 
 --
 -- Indexes for dumped tables
@@ -238,58 +313,58 @@ ALTER TABLE `volunteers`
 --
 
 --
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `AID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `donations`
+--
+ALTER TABLE `donations`
+  MODIFY `DNID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `donationtransaction`
+--
+ALTER TABLE `donationtransaction`
+  MODIFY `DTID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `donors`
+--
+ALTER TABLE `donors`
+  MODIFY `DID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `ngo`
+--
+ALTER TABLE `ngo`
+  MODIFY `NID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `ngoevents`
+--
+ALTER TABLE `ngoevents`
+  MODIFY `EID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `ngoeventspartcpn`
+--
+ALTER TABLE `ngoeventspartcpn`
+  MODIFY `EPID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `UID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- Constraints for dumped tables
---
-
---
--- Constraints for table `admins`
---
-ALTER TABLE `admins`
-  ADD CONSTRAINT `admins_ibfk_1` FOREIGN KEY (`AID`) REFERENCES `users` (`UID`);
-
---
--- Constraints for table `donations`
---
-ALTER TABLE `donations`
-  ADD CONSTRAINT `donations_ibfk_1` FOREIGN KEY (`NID`) REFERENCES `ngo` (`NID`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `donationtransaction`
---
-ALTER TABLE `donationtransaction`
-  ADD CONSTRAINT `donationtransaction_ibfk_1` FOREIGN KEY (`DNID`) REFERENCES `donations` (`DNID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `donationtransaction_ibfk_2` FOREIGN KEY (`DID`) REFERENCES `donors` (`DID`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `donors`
---
-ALTER TABLE `donors`
-  ADD CONSTRAINT `donors_ibfk_1` FOREIGN KEY (`DID`) REFERENCES `users` (`UID`);
-
---
--- Constraints for table `ngoevents`
---
-ALTER TABLE `ngoevents`
-  ADD CONSTRAINT `ngoevents_ibfk_1` FOREIGN KEY (`NID`) REFERENCES `ngo` (`NID`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `ngoeventspartcpn`
---
-ALTER TABLE `ngoeventspartcpn`
-  ADD CONSTRAINT `ngoeventspartcpn_ibfk_1` FOREIGN KEY (`EID`) REFERENCES `ngoevents` (`EID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `ngoeventspartcpn_ibfk_2` FOREIGN KEY (`VID`) REFERENCES `volunteers` (`VID`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `volunteers`
+-- AUTO_INCREMENT for table `volunteers`
 --
 ALTER TABLE `volunteers`
-  ADD CONSTRAINT `volunteers_ibfk_1` FOREIGN KEY (`VID`) REFERENCES `users` (`UID`);
+  MODIFY `VID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
