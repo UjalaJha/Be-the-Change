eaad47903417f3ad8ce30065488c0931982d9d8b
Unknown <2016.khushboo.chandnani@ves.ac.in>
2018-10-08 17:26:53 +0530

    init

--- pages/includes/db.php
+++ pages/includes/db.php
@@ -1,6 +1,6 @@
 <?php
 require_once("constants.php");
-
+//hello
 $connection = mysqli_connect(SERVER,USER,PASSWORD,DB);
 if(!$connection){
     echo "Some issue in connecting ".mysqli_error($connection);
