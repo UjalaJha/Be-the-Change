4c648b930557a9c7d3a0b71efa4459cf4256fbf8
Unknown <jhaujala3@gmail.com>
2018-10-09 16:59:57 +0530

    bgoupdate

--- demo.php
+++ demo.php
@@ -1,12 +1,22 @@
 <?php
 require_once("pages/includes/functions.php");
-echo "hello";
+// echo "hello";
 
 $ngo=getngo();
 
 $events=getngoevents();
 
 $donations=getngodonations();
+
+
+$eventsexe=geteventsexe();
+
+$donationsexe=getdonationsexe();
+
+$nid=3;
+$eventsexe=geteventsexe($nid);
+
+$donationsexe=getdonationsexe($nid);
 
 // $nid=3;
 // $desc='Save Old Age Homes';
@@ -15,13 +25,24 @@ $donations=getngodonations();
 // $nid=3;
 // $damt=3000;
 // $decs='COntribute for Healthy India';
-// $addngoevents=addngodonation($damt,$nid,$decs);
+// $addngodonation=addngodonation($damt,$nid,$decs);
+
+// $eid=3;
+// $vid=2;
+// $op='COntribute for Healthy India';
+// $addusertoevent=addusertoevent($eid,$vid);
+
+// $did=2;
+// $dnid=3;
+// $amnt=1300;
+// $adduserdonation=adduserdonation($dnid,$did,$amnt);
 
 
 
-
-echo "<pre>";
-print_r($ngo);
-print_r($events);
-print_r($donations);
+// echo "<pre>";
+// print_r($ngo);
+// print_r($events);
+// print_r($donations);
+// print_r($eventsexe);
+// print_r($donationsexe);
 ?>
--- pages/includes/functions.php
+++ pages/includes/functions.php
@@ -54,4 +54,45 @@ function addngodonation($damt,$nid,$decs){
     checkQueryResult($resultset);
     return $resultset;
 }
+function addusertoevent($eid,$vid){
+    global $connection;
+    $query = "INSERT INTO  ngoeventspartcpn (EID,VID) VALUES ($eid,$vid)";
+    $resultset = mysqli_query($connection,$query);
+    checkQueryResult($resultset);
+    return $resultset;
+}
+function adduserdonation($dnid,$did,$amnt){
+    global $connection;
+    $query = "INSERT INTO  donationtransaction (DNID,DID,AMOUNT) VALUES ($dnid,$did,$amnt)";
+    $resultset = mysqli_query($connection,$query);
+    checkQueryResult($resultset);
+    return $resultset;
+}
+function getdonationsexe($nid=null){
+    global $connection;
+    $query = "SELECT * FROM donationtransaction";
+    if(!empty($nid))
+    {
+        $query = " where DNID=(SELECT EID FROM donations where NID=$nid);";  
+    }
+    $ngo=mysqli_query($connection,$query);
+    if($row=mysqli_fetch_all($ngo)){
+        return($row);
+    }
+}
+function geteventsexe($nid=null){
+    global $connection;
+    $query = "SELECT * FROM ngoeventspartcpn";
+    if(!empty($nid))
+    {
+        $query = " where EID=(SELECT EID FROM ngoevents where NID=$nid);";  
+    }
+    print_r($query);
+    echo "\n\n\n";
+    $ngo=mysqli_query($connection,$query);
+    print_r($ngo);
+    if($row=mysqli_fetch_all($ngo)){
+        return($row);
+    }
+}
 ?>
