b9065ffc2805d3e49ec5aaa33eda7b0eb3cad7f5
Gayatri Belapurkar <gayatri.belapurkar5@gmail.com>
2018-10-09 12:19:37 +0530

    Login done
    Temporary redirection to home page if correct password.

--- pages/includes/loginfunction.php
+++ pages/includes/loginfunction.php
@@ -1,52 +1,45 @@
 <?php
+echo "hello";
 require_once("db.php");
 require_once("functions.php");
 session_start();
-if(isset($_POST["submit"]))
+if(!empty($_POST["email"]))
 {
-    $username=$_POST['username'];
+    echo "hello";
+    $email=$_POST['email'];
     $password=$_POST['password'];
     
-    $username=mysqli_real_escape_string($connection,$username);
+    $email=mysqli_real_escape_string($connection,$email);
     $password=mysqli_real_escape_string($connection,$password);
     
-    $query="SELECT * FROM users WHERE U_EMAIL = '$username'";
-    
+    $query="SELECT * FROM users WHERE U_EMAIL = '$email' and PASSWORD = '$password'";
+    // print_r($query);
+    // exit;
+
     $select_user_details = mysqli_query($connection,$query);
     
 //    proceed if there is data returned otherwise there would be errors
-    
+
     checkQueryResult($select_user_details);
-    
+    // print_r($select_user_details);
+    // exit;
+
     if(mysqli_num_rows($select_user_details)>1){
         die("ERROR 2");
-//        later we would create a error wala page
-//        and will print the error in user friendly manner
-//        header();
-        
+        //not valid if same email exists for mmore than 1 account
     }
-//    if i m coming here that means i have exactly 0/1 row!
-//    $db_password="";
-    
+
     if($row=mysqli_fetch_assoc($select_user_details)){
 //        i have 1 row
-        $db_password = $row['UPASSWORD'];
-        $employee_id = $row['UID'];
-        
+        $db_password = $row['PASSWORD'];
+        $uid = $row['UID'];
+        $_SESSION['UID']=$uid;
+        header("Location: ../../index.php");
     }
     else
     {
         $db_password="";
-        
+       header("Location: ../../login.html");
     }
-    if(password_verify($password,$db_password)){
-        $_SESSION['UID']=$uid;
-        header("Location: ../login.php");
-    }
-    else{
-        die("ERROR 2");
-        
-    }
-    
 }
 ?>
