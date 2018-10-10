26fb0f7d237933cc665dd97852b9ebaf7ad1de28
Unknown <2016.khushboo.chandnani@ves.ac.in>
2018-10-10 15:32:52 +0530

    user profile and more changes

--- js/script.js
+++ js/script.js
@@ -21,5 +21,27 @@ $(".customer-testimonials").owlCarousel({
 //    },
 //    
 });
+$(".owl-one").owlCarousel({
+    items:3,
+    autoplay:true,
+    smartSpeed:700,
+    loop:true,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1,
+            autoplayHoverPause:false,
+        },
+        480:{
+            items:2,
+        },
+        768:{
+            items:3,
+        },
+        
+    },
+    
+
+});   
 
 });
